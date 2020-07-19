9bb4c463774fc217dfe1e13f97889abe924a3506
EvgenyZuchkov <68130608+EvgenyZuchkov@users.noreply.github.com>
2020-07-19 22:24:15 +0300

    Replaced "a" with "NavLink"

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <a href='/Profile'>Profile</a>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-            <a href='/Dialogs'>Messages</a>
-        </div>
-        <div className={s.item}>
-            <a href='/News'>News</a>
-        </div>
-        <div className={s.item}>
-            <a href='/Music'>Music</a>
-        </div>
-        <div className={s.item}>
-            <a href='/Settings'>Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to ='/Profile' activeClassName = {s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to ='/Dialogs' activeClassName = {s.activeLink}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to ='/News' activeClassName = {s.activeLink}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to ='/Music' activeClassName = {s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to ='/Settings' activeClassName = {s.activeLink}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
