6025f7c7456e49f5cacabf2d5cc5e6b292fe7082
EvgenyZuchkov <68130608+EvgenyZuchkov@users.noreply.github.com>
2020-09-03 13:39:08 +0300

    Refacroting

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log()
+}
 
 let state = {
     profilePage: {
@@ -37,7 +39,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostChange,
@@ -49,9 +51,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostChange=newText;
     rerenderEntireTree(state);
 };
+
+export const subscribe = (observer) => {
+    rerenderEntireTree(observer)
+}
 
 export default state;
