20610bc61682ff1c7045db4f3bdf796a428d4be8
EvgenyZuchkov <68130608+EvgenyZuchkov@users.noreply.github.com>
2020-09-07 20:08:47 +0300

    Apply reducer

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./MyPost/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
     let postsElements =
